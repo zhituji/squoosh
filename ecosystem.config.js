f964dab687bff2c9627a09f4e6c1bcd70b196c50
fqye <fqye@yizhanyun.cn>
2021-09-30 16:03:29 +0800

    Use pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'squoosh',
+      script: './node_modules/serve/bin/serve.js',
+      args: ' --config ../../../serve.json ./build',
+      instances: 2,
+      exec_mode: 'cluster',
+      // env: {
+      //   NODE_ENV: 'development',
+
+      // },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    // optionally a second project
+  ],
+};
--- src/shared/prerendered-app/Intro/index.tsx
+++ src/shared/prerendered-app/Intro/index.tsx
@@ -214,6 +214,9 @@ export default class Intro extends Component<Props, State> {
           }}
         >
           制图机单图片处理引擎无需登录，不上传图片到服务器，请放心使用
+          <p style={{ fontSize: '0.8em' }}>
+            如需使用批量处理和图片工作流功能，登录或注册帐号
+          </p>
         </div>
 
         <div class={style.main}>
